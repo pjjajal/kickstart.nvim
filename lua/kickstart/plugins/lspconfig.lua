8311a48b7f87eb5c179419a4c6f6e5977c8b6df4
pjjajal <jajalpurvish@gmail.com>
2024-06-24 18:45:46 -0400

    Added neogit, noice, and ltex lsp.

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -185,6 +185,7 @@ return {
           },
           capabilities = capabilities,
         },
+        ltex = {},
 
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
