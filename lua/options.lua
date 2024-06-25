6f8d973f103a473ecf0b921dc8a42e0b0d240080
pjjajal <jajalpurvish@gmail.com>
2024-06-25 11:51:01 -0400

    Added cheatsheet, ufo folding, and zen mode.

--- lua/options.lua
+++ lua/options.lua
@@ -59,4 +59,8 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Folding
+-- vim.opt.foldenable = false
+-- vim.opt.foldcolumn = "1"
+-- vim.opt.foldmethod = 'indent'
 -- vim: ts=2 sts=2 sw=2 et
